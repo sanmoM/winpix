08d46db871895a8379f29ae4cdedb4b36fd73f40
unknown <subrotomollik243966@gmail.com>
2025-10-19 11:54:57 +0600

    aut

---
+++ resources/js/components/app-sidebar.tsx
@@ -0,0 +1,87 @@
+import { NavFooter } from '@/components/nav-footer';
+import { NavMain } from '@/components/nav-main';
+import { NavUser } from '@/components/nav-user';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { dashboard } from '@/routes';
+import { type NavItem } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
+import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import AppLogo from './app-logo';
+
+export function AppSidebar() {
+    const { auth } = usePage().props;
+    const userRole = auth?.user?.role || 'user';
+
+    const mainNavItems: NavItem[] = [
+        {
+            title: 'Dashboard',
+            href: 'dashboard',
+            icon: LayoutGrid,
+        },
+    ];
+
+    const adminNavItems: NavItem[] = [
+        {
+            title: 'Dashboard',
+            href: '/admin/dashboard',
+            icon: LayoutGrid,
+        },
+        {
+            title: 'Admin Dashboard',
+            href: 'dashboard',
+            icon: LayoutGrid,
+        },
+    ];
+
+    let roleBaseNavItems = [...mainNavItems];
+
+    if (userRole === 'admin') {
+        roleBaseNavItems = [...adminNavItems];
+    }
+
+    const footerNavItems: NavItem[] = [
+        {
+            title: 'Repository',
+            href: 'https://github.com/laravel/react-starter-kit',
+            icon: Folder,
+        },
+        {
+            title: 'Documentation',
+            href: 'https://laravel.com/docs/starter-kits#react',
+            icon: BookOpen,
+        },
+    ];
+
+    return (
+        <Sidebar collapsible="icon" variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href={dashboard()} prefetch>
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarContent>
+                <NavMain items={roleBaseNavItems} />
+            </SidebarContent>
+
+            <SidebarFooter>
+                <NavFooter items={footerNavItems} className="mt-auto" />
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
+}

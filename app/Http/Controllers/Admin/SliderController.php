b52c2c0800288388d9bac5cbc2f8a4b99a867b6a
unknown <subrotomollik243966@gmail.com>
2025-10-26 11:40:21 +0600

    slider

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -3,24 +3,32 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Slider;
 
 class SliderController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
     public function index()
     {
-        //
+        $sliders = Slider::orderBy('id', 'DESC')->get();
+        return Inertia::render('Admin/Slider/Index', [
+            'sliders' => $sliders
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
+
     public function create()
     {
-        //
+        return Inertia::render('Admin/Slider/Create');
     }
 
     /**
@@ -28,12 +36,32 @@ class SliderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+
+            'title' => 'required|string|max:255',
+            'content' => 'required',
+            'bg_image' => 'required|image|max:2048',
+        ]);
+
+       $slider =new Slider();
+       $slider->title = $request->title;
+       $slider->content = $request->content;
+
+       if ($request->hasFile('bg_image')) {
+           $file = $request->file('bg_image')->store('uploads/slider', 'public');
+           $slider->bg_image = $file;
+       }
+
+       $slider->save();
+
+       return redirect()->back()->with('success', 'Slider saved successfully 🎉');
+
     }
 
     /**
      * Display the specified resource.
      */
+
     public function show(string $id)
     {
         //
@@ -44,7 +72,10 @@ class SliderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $slider = Slider::find($id);
+        return Inertia::render('Admin/Slider/Edit', [
+            'slider' => $slider
+        ]);
     }
 
     /**
@@ -52,7 +83,36 @@ class SliderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+
+            'title' => 'required|string|max:255',
+            'content' => 'required',
+            'bg_image' => 'nullable|image|max:2048',
+            'status' => 'required'
+        ]);
+
+       $slider = Slider::find($id);
+       $slider->title = $request->title;
+       $slider->content = $request->content;
+       $slider->status = $request->status;
+
+        if ($request->hasFile('bg_image')) {
+            // Delete old image
+            if ($slider->bg_image && Storage::disk('public')->exists($slider->bg_image)) {
+                Storage::disk('public')->delete($slider->bg_image);
+            }
+
+            // Store new image
+            $file = $request->file('bg_image')->store('uploads/slider', 'public');
+            $slider->bg_image = $file;
+        }
+
+        $slider->save();
+
+        return redirect()
+            ->route('admin.slider.index')
+            ->with('success', 'Slider updated successfully 🎉');
+
     }
 
     /**
@@ -60,6 +120,15 @@ class SliderController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $slider = Slider::find($id);
+        // Delete old image
+        if ($slider->bg_image && Storage::disk('public')->exists($slider->bg_image)) {
+            Storage::disk('public')->delete($slider->bg_image);
+        }
+        $slider->delete();
+
+        return redirect()
+            ->back()
+            ->with('success', 'Slider item deleted successfully 🎉');
     }
 }

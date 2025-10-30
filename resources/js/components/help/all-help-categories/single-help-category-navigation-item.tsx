06bb729f229166bee2bf66fac269ecb0ff855100
anmobd111 <sanmobd111@gmail.com>
2025-10-30 16:34:07 +0600

    single category helps

---
+++ resources/js/components/help/all-help-categories/single-help-category-navigation-item.tsx
@@ -0,0 +1,21 @@
+import { Link } from "@inertiajs/react";
+
+interface SingleHelpCategoryProps {
+    title: string;
+}
+
+const SingleHelpCategoryNavigationItem: React.FC<SingleHelpCategoryProps> = ({ title }) => {
+    return (
+        <Link
+            href={"/single-category-helps"}
+            className="w-full p-4 h-full
+                 border rounded-sm 
+                font-medium
+                  hover:!scale-[1] cursor-pointer flex justify-center items-center bg-bg-primary block"
+        >
+            {title}
+        </Link>
+    );
+};
+
+export default SingleHelpCategoryNavigationItem;

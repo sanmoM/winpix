c5c813b371fd9c66062bb2236b5ce25759535f45
anmobd111 <sanmobd111@gmail.com>
2025-11-08 12:57:45 +0600

    daynamic home page banner

--- resources/js/components/home/banner/home-banner.tsx
+++ resources/js/components/home/banner/home-banner.tsx
@@ -43,7 +43,7 @@ export default function
             <div className="absolute inset-0 bg-black/50 flex items-center justify-center text-white !text-center p-8">
               <div className={``}>
                 <h2 className="title  !text-center text-2xl md:text-5xl lg:text-6xl font-bold">{slide.title}</h2>
-                <p className="subtitle text-sm md:text-lg lg:text-xl mt-2 mb-3 lg:mt-4 md:mt-3 md:mb-4 lg:mb-6 text-gray-200  !text-center">{slide.subtitle}</p>
+                <p className="subtitle text-sm md:text-lg lg:text-xl mt-2 mb-3 lg:mt-4 md:mt-3 md:mb-4 lg:mb-6 text-gray-200  !text-center">{slide.content}</p>
                 <Button text={t("home.banner.btnText")} className='px-10 py-2 md:py-3 btn' />
               </div>
             </div>

07f1c53fba4dd37e3a806d7c8db2b48fd8f7e064
anmobd111 <sanmobd111@gmail.com>
2025-11-10 14:42:14 +0600

    daynamic store and redeem

--- resources/js/pages/quests/single-quest.tsx
+++ resources/js/pages/quests/single-quest.tsx
@@ -1,17 +1,19 @@
-import Brief from '@/components/shared/brief'
-import Guidelines from '@/components/shared/guidelines/guidelines'
-import Prizes from '@/components/shared/prizes/prizes'
 import Status from '@/components/quests/single-quest/status'
 import Banner from '@/components/shared/banner'
+import Brief from '@/components/shared/brief'
 import Button from '@/components/shared/buttons/button'
 import SecondaryButton from '@/components/shared/buttons/secondary-button'
 import Container from '@/components/shared/container'
 import Creator from '@/components/shared/creator'
 import GalleryImageCart from '@/components/shared/gallary-image-cart'
+import Guidelines from '@/components/shared/guidelines/guidelines'
+import Prizes from '@/components/shared/prizes/prizes'
 import Tab from '@/components/shared/tab'
 import useLocales from '@/hooks/useLocales'
 import UserLayout from '@/layouts/user-layout'
 import { cn } from '@/lib/utils'
+import { Quest } from '@/types/quest'
+import { usePage } from '@inertiajs/react'
 import { useState } from 'react'
 
 const images = [
@@ -30,14 +32,17 @@ const images = [
 ]
 
 export default function SingleQuest() {
+    const { quest } = usePage<any>().props;
+
+    console.log(quest)
     const [activeTab, setActiveTab] = useState("brief");
     const { t, direction } = useLocales()
     return (
         <UserLayout>
-            <Banner src="https://cdn.pulsepx.com/photos/111974512/5652113db680a1dda4a6ea1dbd62f158405f9392ad52e5eca32d080d45d11bbe/2048.jpg" containerClass='lg:h-[70vh]' hasOverlay={false}>
+            <Banner src={"/storage/" + quest?.image} containerClass='lg:h-[70vh]' hasOverlay={false}>
                 <div className='w-full h-full flex flex-col justify-center items-center'>
-                    <h1 className='text-2xl md:text-3xl lg:text-4xl font-bold text-white'>Perfectly Centered</h1>
-                    <p className='text-gray-400 mt-4 mb-6'>#Minimalism</p>
+                    <h1 className='text-2xl md:text-3xl lg:text-4xl font-bold text-white'>{quest?.title}</h1>
+                    <p className='text-gray-400 mt-4 mb-6'>#{quest?.category?.name}</p>
                     <div className='grid grid-cols-2 gap-4'>
                         <SecondaryButton text={t('singleQuest.banner.voteText')} />
                         <Button text={t('singleQuest.banner.joinNowText')} className='px-8 py-2 lg:text-sm' />
@@ -56,12 +61,12 @@ export default function SingleQuest() {
                 />
                 <div className={cn('px-2 space-y-14 md:space-y-20 lg:space-y-10', activeTab !== "brief" && "hidden")}>
                     <Status t={t} direction={direction} />
-                    <Brief title={t('singleQuestDetails.brief.title')} />
+                    <Brief title={t('singleQuestDetails.brief.title')} text={quest?.brief} />
                     <Prizes t={t} />
                     <div className='flex flex-col xl:flex-row justify-between gap-14 md:gap-20 lg:gap-0'>
                         <Guidelines t={t} />
                         <div className='className="w-fit lg:w-full md:max-w-md mt-auto mx-auto lg:mx-0'>
-                            <Creator />
+                            <Creator user={quest?.user} />
                         </div>
                     </div>
                 </div>

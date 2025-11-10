e4426941ad15f5aef4a6b8ea4e72fdf528563579
anmobd111 <sanmobd111@gmail.com>
2025-11-10 14:12:11 +0600

    done

--- resources/js/components/home/newest/Newest.tsx
+++ resources/js/components/home/newest/Newest.tsx
@@ -1,14 +1,17 @@
 import { Quest } from "@/types/quest";
 import SectionHeading from "../../shared/SectionHeading";
 import QuestCard from "./components/PhotoCard";
-export default function Newest({ t, newQuest }: { t: any, newQuest: any }) {
+import { Link } from "@inertiajs/react";
+export default function Newest({ t, newQuest }: { t: any, newQuest: Quest[] }) {
     return (
         <div>
             <SectionHeading title={t("home.newest.title")} className=" !text-center" />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-6">
                 {
                     newQuest?.map((item: Quest, index: number) => (
-                        <QuestCard key={index} item={item} />
+                        <Link className="block" href={"/quests/single-quest/" + item.id}>
+                            <QuestCard key={index} item={item} />
+                        </Link>
                     ))
                 }
             </div>

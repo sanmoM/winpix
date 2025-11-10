0ceb05ba63a4652b838f96f200f1ce4c6f259478
anmobd111 <sanmobd111@gmail.com>
2025-11-10 11:34:46 +0600

    slider seeder

---
+++ database/data/slider.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    [
+        'title' => 'Winpix',
+        'subtitle' => 'Winpix',
+        'description' => 'Winpix is a platform that allows you to create and participate in photo contests. It offers a range of features, including a user-friendly interface, customizable templates, and a powerful analytics dashboard. Whether you\'re a beginner or an experienced photographer, Winpix has something for everyone.',
+        'image' => 'uploads/slider/dIvoUkgPpTZwL9Y3YMjNLGu2ki638EfrxpgMnQ2O.jpg',
+    ],
+    [
+        'title' => 'Capture Moments',
+        'subtitle' => 'Photography',
+        'description' => 'Capture Moments allows you to showcase your photography skills with stunning contests and challenges. Share your best shots and win amazing prizes.',
+        'image' => 'uploads/slider/Ft1NxqiJVmD9v9hy2y2efiBQhjJYNkvnAvdDinP4.jpg',
+    ],
+    [
+        'title' => 'Photo Contest',
+        'subtitle' => 'Contest Platform',
+        'description' => 'Join exciting photo contests every week and compete with photographers worldwide. Gain recognition and rewards for your creativity.',
+        'image' => 'uploads/slider/WxQQOm5S19ySM9HXBRwCkqGO7v128yD7V87jPxIq.jpg',
+    ],
+];

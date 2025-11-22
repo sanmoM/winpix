74d14c1ff9b640a611464cac5ea38e7543515b15
unknown <subrotomollik243966@gmail.com>
2025-11-22 16:38:57 +0600

    done

--- resources/js/components/about-us/what-we-do/what-we-do.tsx
+++ resources/js/components/about-us/what-we-do/what-we-do.tsx
@@ -1,19 +1,9 @@
 import SectionHeading from '@/components/shared/SectionHeading';
 import useLocales from '@/hooks/useLocales';
-import { FaTrophy } from 'react-icons/fa';
-import { IoMdCamera } from 'react-icons/io';
-import { PiBalloonFill } from 'react-icons/pi';
 import ServiceCard from './components/service-card';
 
 const WhatWeDo = ({ t, services }: { t: any }) => {
-    // const services = t('aboutUs.whatWeDo.services', { returnObjects: true });
-    console.log(services);
     const { currentLanguage } = useLocales();
-    const icons = [
-        <FaTrophy className="text-2xl text-white" />,
-        <IoMdCamera className="text-2xl text-white" />,
-        <PiBalloonFill className="text-2xl text-white" />,
-    ];
 
     return (
         <div>

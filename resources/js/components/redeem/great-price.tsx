56ce7b31e3412025114f48e883e7dea63d678b7e
anmobd111 <sanmobd111@gmail.com>
2025-11-24 17:49:17 +0600

    ok

--- resources/js/components/redeem/great-price.tsx
+++ resources/js/components/redeem/great-price.tsx
@@ -5,7 +5,7 @@ import RedeemCard from "./components/redeem-card";
 export default function GreatPrice({ t, prizes }: { t: any, prizes: any }) {
     return (
         <div>
-            <SectionHeading title={t('redeem.appPriceTitle')} />
+            <SectionHeading title={t('redeem.greatPriceTitle')} />
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 sm:gap-6">
                 {prizes.map((prize: any) => (
                     <RedeemCard src={prize.icon_image} title={prize.title} description={prize.description} />

9a6ee7ec2dd8adc5191734b511bdfc520d5bc963
anmobd111 <sanmobd111@gmail.com>
2025-12-17 14:09:47 +0600

    done

--- resources/js/components/shared/card/card.tsx
+++ resources/js/components/shared/card/card.tsx
@@ -51,7 +51,20 @@ export default function Card({ item, isSeries = false }: any) {
                                 } USD</span>
                         </div>
                     </div>
-                    <span className="text-white bg-white/10 px-4 py-1 rounded-full backdrop-blur-[3px] text-[11px] font-semibold tracking-wider">@{item?.user?.name}</span>
+                    <div className="flex flex-col items-center">
+                        {!isSeries && (<div className="flex items-center gap-1 border border-primary px-4 py-1.5 rounded-full mb-2 text-xs">
+                            <img src={"/images/coin.png"} alt="Old Crane in Gdansk at Night" className="w-4 h-4 rounded-full" />
+                            <p>X {item?.entry_coin}</p>
+                        </div>)
+                        }
+                        <span className="text-white bg-white/10 px-4 py-1 rounded-full backdrop-blur-[3px] text-[11px] font-semibold tracking-wider">
+                            {
+                                isSeries ? <span>@{item?.user?.name}</span> : <span>
+                                    # {item?.rank_tier}
+                                </span>
+                            }
+                        </span>
+                    </div>
                 </div>
             </div>
 

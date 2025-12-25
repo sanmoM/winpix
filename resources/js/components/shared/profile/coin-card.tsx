eaef7b25619ea5b5ad6fd764c2e150585e30fe34
anmobd111 <sanmobd111@gmail.com>
2025-12-25 15:59:25 +0600

    commit

--- resources/js/components/shared/profile/coin-card.tsx
+++ resources/js/components/shared/profile/coin-card.tsx
@@ -4,7 +4,7 @@ export default function CoinCard({ item }: { item: any }) {
     return (
         <div className=' bg-bg-primary p-4 rounded-sm'>
             <img src={item?.src} alt="hasmonaut's profile" className="w-10 h-10 rounded-full object-cover object-top mx-auto" />
-            <p className='text-2xl font-bold !text-center mt-2'>100</p>
+            <p className='text-2xl font-bold !text-center mt-2'>{item?.count}</p>
         </div>
     )
 }

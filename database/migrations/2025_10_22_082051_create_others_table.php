5b98a850afaf8d2cd3f9a2196df508aaf86b0ade
anmobd111 <sanmobd111@gmail.com>
2025-12-30 12:14:55 +0600

    done

--- database/migrations/2025_10_22_082051_create_others_table.php
+++ database/migrations/2025_10_22_082051_create_others_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,11 +12,17 @@ return new class extends Migration
     {
         Schema::create('others', function (Blueprint $table) {
             $table->id();
-            $table->string('logo');
+            $table->string('light_logo');
+            $table->string('dark_logo');
             $table->string('fav_icon');
+            $table->string('app_name');
             $table->text('app_description');
             $table->longText('terms_of_service');
             $table->longText('privacy_policy');
+            $table->longText('copyright');
+            $table->longText('facebook');
+            $table->longText('twitter');
+            $table->longText('instagram');
             $table->timestamps();
         });
     }

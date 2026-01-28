cf8c2beab248bd3413f8b7fefd007ae67489b8d1
anmobd111 <sanmobd111@gmail.com>
2026-01-28 09:55:57 +0600

    done

--- resources/js/components/shared/buttons/button.tsx
+++ resources/js/components/shared/buttons/button.tsx
@@ -7,7 +7,7 @@ export default function Button({ text, className, hasIcon, src, onClick, type = 
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={cn(" disabled:cursor-not-allowed px-4 py-2 text-sm font-semibold !text-white hover:scale-105 duration-300 rounded-full transition cursor-pointer ease-in-out bg-gradient-to-r bg-[linear-gradient(45deg,var(--color-primary-color),var(--color-secondary-color))]  flex items-center justify-center gap-2 mx-auto", className)}>
+            className={cn("disabled:bg-gray-500 disabled:bg-[url('')] disabled:hover:!scale-100 disabled:cursor-not-allowed px-4 py-2 text-sm font-semibold !text-white hover:scale-105 duration-300 rounded-full transition cursor-pointer ease-in-out bg-gradient-to-r bg-[linear-gradient(45deg,var(--color-primary-color),var(--color-secondary-color))]  flex items-center justify-center gap-2 mx-auto", className)}>
             {
                 loading ? (
                     <TbLoader2 className="w-6 h-6 animate-spin" />
